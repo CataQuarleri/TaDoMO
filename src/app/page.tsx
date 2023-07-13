18b8724e42d3f8ff32119856e1748f7f3bd85d15
CataQu <cataquarleri@gmail.com>
2023-07-13 13:05:43 -0600

    vercel trial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+     <h1 style={{color: 'white'}}>TA DO Maintenance Outsourcing</h1>
     </main>
   )
 }
